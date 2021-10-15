1b9e54693116623440f4e2ab47a9485d1c193a24
AlexRazgulyaev <wilddogdingo@yandex.ru>
2021-10-16 00:43:45 +0300

    feature: подключен script открытия и редактирования popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,43 @@
-let editProfile = document.querySelector('.profile__edit-button');
-let popup = document.querySelector('.popup');
+const buttonEditProfile = document.querySelector('.profile__edit-button');
+const buttonCloseProfile = document.querySelector('.popup__close');
+const popupElement = document.querySelector('.popup');
+const formElement = document.querySelector('.popup__container');
+const nameInput = formElement.querySelector('.popup__name');
+const jobInput = formElement.querySelector('.popup__about-me');
+const nameElement = document.querySelector('.profile__name');
+const jobElement = document.querySelector('.profile__about-me');
 
-function popupOpen() {
-  popup.classList.add('popup_opened');
-  console.log(popup.classList);
+
+function openPopup() {
+  popupElement.classList.add('popup_opened');
 }
-editProfile.addEventListener('click',popupOpen);
-
-let closeProfile = document.querySelector('.popup__close');
-
-function popupClose() {
-  popup.classList.remove('popup_opened');
-  console.log(popup.classList);
+function closePopup() {
+  popupElement.classList.remove('popup_opened');
 }
-closeProfile.addEventListener('click', popupClose);
+function closePopupWithoutSave() {
+  if (nameInput.value === nameElement&&jobInput.value === jobElement) {
+    closePopup;
+  }
+  else {
+    nameInput.value = nameElement.textContent;
+    jobInput.value = jobElement.textContent;
+    closePopup;
+  }
+}
+
+buttonEditProfile.addEventListener('click',openPopup);
+buttonCloseProfile.addEventListener('click', closePopupWithoutSave);
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+
+    const nameValue = nameInput.value;
+    const jobValue = jobInput.value;
+
+    nameElement.textContent =  nameValue;
+    jobElement.textContent = jobValue;
+    closePopup();
+}
+formElement.addEventListener('submit', formSubmitHandler);
+
+
