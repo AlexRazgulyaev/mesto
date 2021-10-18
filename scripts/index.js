f2d93e31e4871bfde021c6aace62914411554b01
AlexRazgulyaev <wilddogdingo@yandex.ru>
2021-10-19 00:52:37 +0300

    JS: Исправлен код открытия и закрытия popup

--- scripts/index.js
+++ scripts/index.js
@@ -1,43 +1,28 @@
-const buttonEditProfile = document.querySelector('.profile__edit-button');
-const buttonCloseProfile = document.querySelector('.popup__close');
-const popupElement = document.querySelector('.popup');
-const formElement = document.querySelector('.popup__container');
-const nameInput = formElement.querySelector('.popup__name');
-const jobInput = formElement.querySelector('.popup__about-me');
-const nameElement = document.querySelector('.profile__name');
-const jobElement = document.querySelector('.profile__about-me');
-
+const buttonEditProfile = document.querySelector(".profile__edit-button");
+const buttonCloseProfile = document.querySelector(".popup__close");
+const popupElement = document.querySelector(".popup");
+const formElement = document.querySelector(".popup__container"); // форма
+const nameInput = formElement.querySelector(".popup__input_type_name"); // name в input
+const jobInput = formElement.querySelector(".popup__input_type_job"); // job в input
+const nameElement = document.querySelector(".profile__name"); // name на сайте
+const jobElement = document.querySelector(".profile__about-me"); // job на сайте
 
 function openPopup() {
-  popupElement.classList.add('popup_opened');
+  popupElement.classList.add("popup_opened");
+  nameInput.value = nameElement.textContent; // значения с сайта переносим в input
+  jobInput.value = jobElement.textContent;
 }
 function closePopup() {
-  popupElement.classList.remove('popup_opened');
-}
-function closePopupWithoutSave() {
-  if (nameInput.value === nameElement&&jobInput.value === jobElement) {
-    closePopup;
-  }
-  else {
-    nameInput.value = nameElement.textContent;
-    jobInput.value = jobElement.textContent;
-    closePopup;
-  }
+  popupElement.classList.remove("popup_opened");
 }
 
-buttonEditProfile.addEventListener('click',openPopup);
-buttonCloseProfile.addEventListener('click', closePopupWithoutSave);
-
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-
-    const nameValue = nameInput.value;
-    const jobValue = jobInput.value;
-
-    nameElement.textContent =  nameValue;
-    jobElement.textContent = jobValue;
-    closePopup();
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  nameElement.textContent = nameInput.value; // значения из input переносим на сайт
+  jobElement.textContent = jobInput.value;
+  closePopup();
 }
-formElement.addEventListener('submit', formSubmitHandler);
 
-
+buttonEditProfile.addEventListener("click", openPopup);
+formElement.addEventListener("submit", formSubmitHandler);
+buttonCloseProfile.addEventListener("click", closePopup);
