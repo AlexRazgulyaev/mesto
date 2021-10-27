5525b5ec2d8231676ace98318b15f62c2e467f91
AlexRazgulyaev <wilddogdingo@yandex.ru>
2021-10-28 00:27:10 +0300

    feat: фото подгружаются js

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,69 @@ const nameInput = formElement.querySelector(".popup__input_type_name"); // name 
 const jobInput = formElement.querySelector(".popup__input_type_job"); // job в input
 const nameElement = document.querySelector(".profile__name"); // name на сайте
 const jobElement = document.querySelector(".profile__about-me"); // job на сайте
+const initialCards = [                                           // массив фотографий
+  {
+    name: "Архыз",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    name: "Челябинская область",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    name: "Иваново",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    name: "Камчатка",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    name: "Холмогорский район",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    name: "Байкал",
+    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
+const cardContainer = document.querySelector(".cards"); // получили контейнер
+const cardTemplate = document.querySelector("#card-template").content; // получили template
+
+const cardImageElement = cardTemplate.querySelector(".cards__image"); // находим в template фото
+const cardTitleElement = cardTemplate.querySelector(".cards__title"); // находим в template заголовок
+
+// for (let i = 0; i <= initialCards.length; i++) {
+//   cardTitleElement.textContent = initialCards[i]['name'];
+//   cardImageElement.src = initialCards[i].link;
+
+
+//   let cardElement = cardTemplate.querySelector(".cards__card").cloneNode(true);
+
+//   cardContainer.append(cardElement);
+// }
+
+initialCards.forEach(function(item) {
+  cardTitleElement.textContent = item.name; // присвоили из массива заголовок
+  cardImageElement.src = item.link;         // присвоили из массива ссылку на фото
+  cardImageElement.alt = item.name;         // присвоили из массива alt фото
+  const cardElement = cardTemplate.querySelector(".cards__card").cloneNode(true); // клонировали c вложением
+
+  cardContainer.append(cardElement);                  // вставили в конец
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function openPopup() {
   popupElement.classList.add("popup_opened");
@@ -22,6 +85,8 @@ function formSubmitHandler(evt) {
   jobElement.textContent = jobInput.value;
   closePopup();
 }
+
+
 
 buttonEditProfile.addEventListener("click", openPopup);
 formElement.addEventListener("submit", formSubmitHandler);
