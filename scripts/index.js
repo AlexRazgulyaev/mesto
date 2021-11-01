ad5da38e4074c8102cd1243726d370b821c0c99e
AlexRazgulyaev <wilddogdingo@yandex.ru>
2021-11-01 21:43:51 +0300

    review: исправлены ошибки index.js

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,7 @@ const jobInput = formEditProfileElement.querySelector(".popup__input_type_job");
 const buttonAddPhoto = document.querySelector(".profile__add-button");             // кнопка добавления фото
 const popupAddPhotoElement = document.querySelector(".popup_type_add-photo");      // popup добавления фото
 const buttonCloseAddPhoto = popupAddPhotoElement.querySelector(".popup__close");   // кнопка закрытия popup
-const formAddPhotoElement = popupAddPhotoElement.querySelector(".popup__container");  // форма добавления фото
+const formAddPhotoElement = popupAddPhotoElement.querySelector(".popup__container");       // форма добавления фото
 const namePlaceInput = formAddPhotoElement.querySelector(".popup__input_type_name-place"); // название места в input
 const linkInput = formAddPhotoElement.querySelector(".popup__input_type_link");            // ссылка на место в input
 const popupPhoto = document.querySelector(".popup_type_image");                    // popup фото
@@ -16,46 +16,20 @@ const photoElement = popupPhoto.querySelector(".popup__image");                 
 const photoTitleElement = popupPhoto.querySelector(".popup__photo-title");         // элемент с подписью
 const nameElement = document.querySelector(".profile__name");                      // name на сайте
 const jobElement = document.querySelector(".profile__about-me");                   // job на сайте
-const initialCards = [                                                             // массив фотографий
-  {
-    name: "Архыз",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
-  },
-  {
-    name: "Челябинская область",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
-  },
-  {
-    name: "Иваново",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
-  },
-  {
-    name: "Камчатка",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-  },
-  {
-    name: "Холмогорский район",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-  },
-  {
-    name: "Байкал",
-    link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
-];
-const cardContainer = document.querySelector(".cards"); // получили контейнер
-const cardTemplate = document.querySelector(".card-template").content; // получили template
-const cardImageElement = cardTemplate.querySelector(".cards__image"); // находим в template фото
-const cardTitleElement = cardTemplate.querySelector(".cards__title"); // находим в template заголовок
+const cardContainer = document.querySelector(".cards");                            // получили контейнер
+const cardTemplate = document.querySelector(".card-template").content;             // получили template
 
-function main() {
+
+function renderCards() {
   initialCards.forEach((element) => {           // прошли по всем элементам массива
     renderPhoto(element.name, element.link);
   });
-
-  formAddPhotoElement.addEventListener("submit", formAddSubmitHandler);  // слушаем кнопку добавить фото
 }
 
-function renderPhoto(name, link) {
+function createCard(name, link) {
+  const cardImageElement = cardTemplate.querySelector(".cards__image");  // находим в template фото
+  const cardTitleElement = cardTemplate.querySelector(".cards__title");  // находим в template заголовок
+
   cardTitleElement.textContent = name; // присвоили из массива заголовок
   cardImageElement.src = link; // присвоили из массива ссылку на фото
   cardImageElement.alt = name; // присвоили из массива alt фото
@@ -65,15 +39,25 @@ function renderPhoto(name, link) {
   cardElement.querySelector(".cards__like").addEventListener("click", likePhotoHandler); // слушаем клик по элементу like
   cardElement.querySelector(".cards__delete").addEventListener("click", deletePhotoHandler); // слушаем клик по элементу delete
   cardElement.querySelector(".cards__image").addEventListener("click", openPhoto);   // слушаем клик по картинке
-  cardContainer.prepend(cardElement); // вставили в начало контейнера
+
+  return cardElement;
+}
+
+function renderPhoto(name, link) {
+  cardContainer.append(createCard(name, link)); // отрисовали фото в конец контейнера
+}
+
+function renderNewPhoto(name, link) {           //отрисовали добавленное фото в начало контейнера
+  cardContainer.prepend(createCard(name, link));
 }
 
 function openPopup(popupElement) {
   popupElement.classList.add("popup_opened");
-  if (popupElement === popupEditProfileElement) {        // если popup редактирования профиля
-    nameInput.value = nameElement.textContent;           // то значения с сайта переносим в input
-    jobInput.value = jobElement.textContent;
-  }
+}
+function openPopupProfile(popupElement) {    // открытие popup с профилем
+  nameInput.value = nameElement.textContent; // то значения с сайта переносим в input
+  jobInput.value = jobElement.textContent;
+  openPopup(popupElement);
 }
 function closePopup(popupElement) {
   popupElement.classList.remove("popup_opened");
@@ -88,8 +72,9 @@ function formAddSubmitHandler(evt) {
   evt.preventDefault();
   const name = namePlaceInput.value;      // значения из input добавления фото
   const link = linkInput.value;
-  renderPhoto(name, link);                // отрисовываем фото
+  renderNewPhoto(name, link);                // отрисовываем фото
   closePopup(popupAddPhotoElement);
+  formAddPhotoElement.reset();
 }
 function likePhotoHandler(evt) {
  evt.target.closest(".cards__like").classList.toggle("cards__like_active");  //находим ближайшей элемент like и переключаем модификатор
@@ -98,6 +83,7 @@ function deletePhotoHandler(evt) {
   evt.target.closest(".cards__card").remove();   //находим ближайшую карточку и удаляем
 }
 function openPhoto(evt) {
+  openPopup(popupPhoto);
   popupPhoto.classList.add("popup_opened");
   const photo = evt.target.closest(".cards__image");                                // находим ближайшую картинку
   const title = evt.target.closest(".cards__card").querySelector(".cards__title");  // находим ближайший текст картинки
@@ -106,21 +92,22 @@ function openPhoto(evt) {
   photoTitleElement.textContent = title.textContent;   // элементу с подписью присваиваем текст
 }
 
-buttonEditProfile.addEventListener("click", function () {
-  openPopup(popupEditProfileElement);
+buttonEditProfile.addEventListener("click", () => {
+  openPopupProfile(popupEditProfileElement);
 });
 formEditProfileElement.addEventListener("submit", formProfileSubmitHandler);
-buttonAddPhoto.addEventListener("click", function () {
+buttonAddPhoto.addEventListener("click", () => {
   openPopup(popupAddPhotoElement);
 });
-buttonCloseProfile.addEventListener("click", function () {
+formAddPhotoElement.addEventListener("submit", formAddSubmitHandler);  // слушаем кнопку добавить фото
+buttonCloseProfile.addEventListener("click", () => {
   closePopup(popupEditProfileElement);
 });
-buttonCloseAddPhoto.addEventListener("click", function () {
+buttonCloseAddPhoto.addEventListener("click", () => {
   closePopup(popupAddPhotoElement);
 });
-buttonClosePhoto.addEventListener("click", function() {
+buttonClosePhoto.addEventListener("click", () => {
   closePopup(popupPhoto);
 })
 
-main();
+renderCards();
